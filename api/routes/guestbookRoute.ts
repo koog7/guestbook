bd90b3158a69f5fb5ad7bd9af48771033a3a5ae9
koog7 <k.ognev709@mail.com>
2024-08-14 14:04:31 +0600

    Добавлен post метод с валидацией автора и фотографии

--- api/routes/guestbookRoute.ts
+++ api/routes/guestbookRoute.ts
@@ -4,7 +4,13 @@ const guestbookRouter = express.Router();
 guestbookRouter.use(express.json());
 
 guestbookRouter.post('/messages', async (req, res) => {
-    res.send('Test')
-});
+    const { author = 'Anonymous', message, photo = '' } = req.body;
 
+    if (!message) {
+        return res.status(400).send({ error: 'Message is required' }); // Проверка обязательного поля
+    }
+
+    res.send({ author, message, photo });
+
+});
 export default guestbookRouter;
